99c40753a32dc0a64654c644352bce5979326367
Alexandra Ligacheva <aligacheva@ARTGROUP.local>
2020-02-25 18:58:14 +0300

    feat: add transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,37 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  const finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        if (i === arr.length - 1) {
+          break;
+        }
+        i += 1;
+        break;
+      case "--discard-prev":
+        if (i > 0) {
+          finalArr.pop();
+          break;
+        }
+        break;
+      case "--double-next":
+        if (i === arr.length - 1) {
+          break;
+        }
+        finalArr.push(arr[i + 1]);
+        break;
+      case "--double-prev":
+        if (i > 0) {
+          finalArr.push(arr[i - 1]);
+          break;
+        }
+        break;
+      default:
+        finalArr.push(arr[i]);
+    }
+  }
+  return finalArr;
 };
