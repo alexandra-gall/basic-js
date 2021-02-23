47118f2c38d3f94e368ff41bb44fe78e9505bc00
Alexandra Ligacheva <alexandra-gall@mail.ru>
2021-02-23 23:23:01 +0300

    fix: fix transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,37 +1,46 @@
+const sequences = {
+  "--discard-next": 1,
+  "--discard-prev": 2,
+  "--double-next": 3,
+  "--double-prev": 4,
+};
+
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error();
   }
+
   const finalArr = [];
+
   for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case "--discard-next":
-        if (i === arr.length - 1) {
+    if (sequences[arr[i]]) {
+      switch (sequences[arr[i]]) {
+        case 1:
+          if (i < arr.length) {
+            finalArr.push(undefined);
+            i += 1;
+          }
           break;
-        }
-        i += 1;
-        break;
-      case "--discard-prev":
-        if (i > 0) {
-          finalArr.pop();
+        case 2:
+          if (i > 0) {
+            finalArr.pop();
+          }
           break;
-        }
-        break;
-      case "--double-next":
-        if (i === arr.length - 1) {
+        case 3:
+          if (i < arr.length) {
+            finalArr.push(arr[i + 1]);
+          }
           break;
-        }
-        finalArr.push(arr[i + 1]);
-        break;
-      case "--double-prev":
-        if (i > 0) {
-          finalArr.push(arr[i - 1]);
+        case 4:
+          if (i > 0) {
+            finalArr.push(finalArr[finalArr.length - 1]);
+          }
           break;
-        }
-        break;
-      default:
-        finalArr.push(arr[i]);
+      }
+    } else {
+      finalArr.push(arr[i]);
     }
   }
-  return finalArr;
-}
+
+  return finalArr.filter((item) => item !== undefined);
+};
